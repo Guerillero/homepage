8bfc2fc7aa842038b53b85f0d091b6834ff26f76
Tom Fish <guerillero.wikipedia@gmail.com>
2025-06-30 21:52:33 +0300

    tajikistan is far too small as a thumb

--- thumb.py
+++ thumb.py
@@ -5,5 +5,8 @@ for image in os.listdir("img"):
     if not ("thumb" in image):
         fin = Image.open(os.path.join("img", image))
         width, height = fin.size
-        fin.thumbnail((width * 0.3, height * 0.3))
+        if width > 1000:
+            fin.thumbnail((width * 0.2, height * 0.2))
+        else:
+            fin.thumbnail((width, height))
         fin.save(os.path.join("img", "thumb_" + image))
