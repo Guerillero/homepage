e519c83dc0f63e35ca73428c98b558e5581017a0
Tom Fish <guerillero.wikipedia@gmail.com>
2025-06-30 21:43:14 +0300

    gahhh

--- thumb.py
+++ thumb.py
@@ -5,5 +5,5 @@ for image in os.listdir("img"):
     if not ("thumb" in image):
         fin = Image.open(os.path.join("img", image))
         width, height = fin.size
-        fin.thumbnail((width * 0.1, height * 0.1))
+        fin.thumbnail((width * 0.3, height * 0.3))
         fin.save(os.path.join("img", "thumb_" + image))
